6cd24897856f8db655a065d04ff35c0914048495
Justin LaRose <justinplarose@gmail.com>
2017-11-12 21:34:07 -0500

    First Commit for the demo

---
+++ Demo_GitKraken/Demo_GitKraken/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_GitKraken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string justinsResponse;
+            Console.WriteLine("Justin's first edit and check in, please type something in:");
+            justinsResponse = Console.ReadLine();
+            Console.WriteLine("You entered: {0}", justinsResponse);
+
+            // Bob's edit for a pull request
+
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadLine();
+            return;
+            
+        }
+    }
+}
